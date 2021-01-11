eb990bd37944a234ce96b95aa5d329375bf06e18
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-11 02:21:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,7 +6,22 @@ collection.find do |e|
 end
 end
 
-
+def consolidate_cart(cart)
+  new_cart = []
+  cart.map do |og_e|
+    if !find_item_by_name_in_collection(og_e[:item], new_cart)
+      new_cart.push(og_e)
+      og_e[:count] = 1
+    else
+      new_cart.map do |e|
+      if og_e[:item] == e[:item]
+      e[:count] += 1
+      end
+    end
+  end
+end
+print new_cart
+end
 
 
 
